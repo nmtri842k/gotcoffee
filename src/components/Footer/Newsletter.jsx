39a956cc4f847e3c1987e392cd57d41c667202b7
nmtri842k <106291954+nmtri842k@users.noreply.github.com>
2023-02-02 11:47:26 +0700

    update-new-emailjs

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import SubHeading from '../SubHeading/SubHeading';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import emailjs from '@emailjs/browser';
 
 import './Newsletter.css';
 
@@ -13,7 +14,6 @@ const Newsletter = () => {
   }
 
 
-
   const checkEmail = (email) => {
     let regexEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
     if (!regexEmail.test(email)) {
@@ -23,56 +23,80 @@ const Newsletter = () => {
     }
   }
 
-  const handleSubmit = () => {
-    if (textInput === '') {
-      toast.error('Vui lòng điền đầy đủ thông tin', {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-    else if (checkEmail(textInput)) {
-      if (window.Email) {
-        window.Email.send({
-          SecureToken: "42798a29-c424-4e16-8a74-3ac877963d1e",
-          Host: "smtp.elasticemail.com",
-          Username: "nmtri842k@gmail.com",
-          Password: "4F9406B500F8F2346CB2D3AEA29AA73EBB52",
-          Port: 2525,
-          To: `${textInput}`,
-          // To: "devil.tri.no1@gmail.com",
-          From: 'nmtri842k@gmail.com',
-          Subject: "This is the subject",
-          Body: "And this is the body"
-        }).then(() => toast.success(`Bạn đã gửi thành công với Email ${textInput}`, {
-          position: "top-right",
-          autoClose: 2000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        }))
-      }
-    } else {
-      toast.error('Email sai định dạng', {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-  }
+  // const handleSubmit = () => {
+  //   if (textInput === '') {
+  //     toast.error('Vui lòng điền đầy đủ thông tin', {
+  //       position: "top-right",
+  //       autoClose: 2000,
+  //       hideProgressBar: false,
+  //       closeOnClick: true,
+  //       pauseOnHover: true,
+  //       draggable: true,
+  //       progress: undefined,
+  //       theme: "light",
+  //     });
+  //   }
+  //   else if (checkEmail(textInput)) {
+  //     emailjs.sendForm('service_j5453tf', 'template_zwnynfl', textInput, '-GnoLdQ8y06CLWfhF')
+  //       .then((result) => {
+  //         console.log(result.text);
+  //       }, (error) => {
+  //         console.log(error.text);
+  //       });
+  //   }
+  //   else {
+  //     toast.error('Email sai định dạng', {
+  //       position: "top-right",
+  //       autoClose: 2000,
+  //       hideProgressBar: false,
+  //       closeOnClick: true,
+  //       pauseOnHover: true,
+  //       draggable: true,
+  //       progress: undefined,
+  //       theme: "light",
+  //     });
+  //   }
+  // }
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_j5453tf",
+        "template_zwnynfl",
+        form.current,
+        "-GnoLdQ8y06CLWfhF"
+      )
+      .then(
+        (result) => {
+          toast.success(`${result.text}`, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+
+        },
+        (error) => {
+          toast.error(`${error.text}`, {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      );
+  };
+
 
   return (
     <div className='app__newsletter'>
@@ -81,9 +105,18 @@ const Newsletter = () => {
         <h1 className="headtext__cormorant">Subscribe To Our Email</h1>
         <p className="p__opensans">Đừng quên bản cập nhật mới nhất!</p>
       </div>
-      <div className="app__newsletter-input flex__center">
+      {/* <div className="app__newsletter-input flex__center">
         <input type="email" placeholder='Nhập email của bạn' onChange={handleChange} />
         <button className='custom__button' onClick={handleSubmit}>Subcirbe</button>
+      </div> */}
+      <div className="app__newsletter-input flex__center">
+        <form ref={form} onSubmit={sendEmail}>
+          <label><p className="p__opensans">Name</p></label>
+          <input type="text" name="user_name" />
+          <label><p className="p__opensans">Email</p></label>
+          <input type="email" name="user_email" />
+          <button className='custom__button'>Sent</button>
+        </form>
       </div>
       <ToastContainer
         position="top-right"
